28d0acd5de31ad7c4c78a6237460211bc737e4c3
Brendan Whiting <brendan.whiting@me.com>
2016-07-24 12:05:36 -0400

    refactored with consistent jquery and no unnecessary splitting search terms

--- app.js
+++ app.js
@@ -12,37 +12,6 @@ if(typeof(String.prototype.trim) === "undefined")
     };
 }
 
-// Warn if overriding existing method
-if(Array.prototype.equals)
-    console.warn("Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.");
-// attach the .equals method to Array's prototype to call it on any array
-Array.prototype.equals = function (array) {
-    // if the other array is a falsy value, return
-    if (!array)
-        return false;
-
-    // compare lengths - can save a lot of time 
-    if (this.length != array.length)
-        return false;
-
-    for (var i = 0, l=this.length; i < l; i++) {
-        // Check if we have nested arrays
-        if (this[i] instanceof Array && array[i] instanceof Array) {
-            // recurse into the nested arrays
-            if (!this[i].equals(array[i]))
-                return false;       
-        }           
-        else if (this[i] != array[i]) { 
-            // Warning - two different object instances will never be equal: {x:20} != {x:20}
-            return false;   
-        }           
-    }       
-    return true;
-};
-// Hide method from for-in loops
-Object.defineProperty(Array.prototype, "equals", {enumerable: false});
-
-
 /********************************************************* 
 
     My code
@@ -87,7 +56,12 @@ $(document).ready(function() {
 
         if (numPages > 1) {
             for (var i = 0; i < numPages; i++) {
-                pages.append('<li><a class="page-number" data-index="' + i + '">' + (i + 1) + '</a></li>');
+                var page_number = $('<li></li>')
+                                        .append('<a></a>')
+                                        .addClass('page-number')
+                                        .attr('data-index', i)
+                                        .text(i + 1);
+                pages.append(page_number);
             }
         }
 
@@ -128,21 +102,18 @@ $(document).ready(function() {
     var new_search_terms;
 
     $('.student-search').find('input').keyup(function() {
-        new_search_terms = $(this).val().toLowerCase().trim().split(" ");
+        new_search_terms = $(this).val().toLowerCase().trim();
 
         console.log(new_search_terms);
         console.log(current_search_terms);
-        console.log(new_search_terms.equals(current_search_terms));
+        console.log(new_search_terms === current_search_terms);
 
-        if (new_search_terms.equals(current_search_terms) === false) {  // don't run the function again if nothing's changed
+        if (new_search_terms !== current_search_terms) {  // don't run the function again if nothing's changed
             var filtered_students = student_list.filter(function(i) {
-                var student_names = $(student_list[i]).find('h3')[0].innerHTML.split(" ");
-                for (var j = 0; j < student_names.length; j++) {
-                    for (var k = 0; k < new_search_terms.length; k++) {
-                        if (student_names[j].indexOf(new_search_terms[k]) > -1) {
-                            return true;
-                        }
-                    }
+                var student_names = $(this).find('h3').text();
+                console.log(student_names);
+                if (student_names.indexOf(new_search_terms) > -1) {
+                    return true;
                 }
                 return false;
             });
